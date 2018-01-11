c26940d5831911057988d2500059331668be0a47
Michael Hammer <michaelhammer@Michaels-MacBook-Pro.local>
2018-01-11 12:28:16 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,34 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
+
+var checkForMatch = function () {
+//note: following lines of code asked to be moved state using console.log versus alert, so alert message box will not appear, but correct string does appear in consol from line 9
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+
+}
+
+var flipCard = function (cardId){
+	
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+flipCard(0);
+flipCard(2);
+
+
+//section 8 code
+
+/*var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped queen");
 var cardTwo = cards[2];
@@ -13,5 +41,5 @@ if (cardsInPlay.length === 2) {
 	} else {
 		alert("Sorry try again.");
 	}
-}
+}*/
 
